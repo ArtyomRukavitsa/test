5bd5cc235a4f70f2649b12c0585e42c99bba1529
Artyom Rukavitsa <rukavitsa.a@yandex.ru>
2021-06-23 15:17:07 +0300

    secret vals

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import streamlit as st
 import sqlite3
+import os
 
 st.title("TEST")
 con = sqlite3.connect('data.db')
 cur = con.cursor()
 st.write(cur.execute("SELECT name FROM addresses").fetchall())
+st.write(os.environ['SOMETHING'])
 cur.close()
 con.close()
